28ab08da8c10cdf9904ca610afafbc568123e55d
Roberto Tamburini <Explosiond723@users.noreply.github.com>
2019-02-13 20:44:42 +0100

    Added first file
    Added first file  with the skeleton of the program

---
+++ chat.py
@@ -0,0 +1,19 @@
+#! /bin/env/ python
+import socket
+import threading
+
+# -------------------------------------------------------------
+# Simil IRC Chat - using standard ports for eluding firewalls
+# -------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------
+# objective:    use http/tcpip ports -> 80, 443, 8080, 8443 for eluding tyical whitelist firewalls
+# ------------+ serverless -> peer 2 peer chat, so it avoids firewalls like above
+#             | RAM chats -> chats are not stored somewhere, so you can also talk about confidential things
+#             +----------------------------------------------------------------------------------------------
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
